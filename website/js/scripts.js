01e79ac35d06077d2f0c185886d4a7d972be66f9
Samuel Hooker <sjh298@uclive.ac.nz>
2017-09-21 12:28:39 +1200

    everything is updated and broken

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,14 +1,60 @@
-$(document).ready(function(){
-    $("#submitCode").click(function(){
-        //$("#codeBox").animate({height: "300px"});
-        $("#codeForm").addClass("header");
-        $("#submitCode").addClass("headerDisappear");
-        $("#codeBox").addClass("headerDisappear");
-    });
-});
 
 
-let socket = new WebSocket("ws://132.181.13.96:2828", "checkingLifeWorks");
+// $(document).ready(function(){
+//     $( "#codeEntryForm" ).on( "c", loadControls());
+//
+// });
+
+var insults = ["probably never play this game again.", "No one has ever sailed as badly as you just did.", "you suck.", "rookie.",
+    "better luck next time. Not that any of your friends will let you sail with them again.", "you died.", "Shutting down device"]
+
+function submitButtonPressed(){
+
+  //TODO: authenitifaction and game connection logic goes here
+
+    if(true){
+        initControls("Emerites Team New Zealand");
+        changeColor("skyblue");
+        loadControls();
+    }
+
+}
+
+function updateStats(speed, placing, health){
+    console.log("updating");
+    $("#boatSpeed").html(speed+"kn");
+    $("#placing").html(placing);
+    $("#boatHealth").html(health+"%");
+}
+
+function initControls(teamName){
+    $("#boatNameText").html(teamName);
+    $("#boatSpeed").html("0kn");
+    $("#placing").html("-");
+    $("#boatHealth").html("100%");
+}
+
+function loadControls(){
+  $("#codeForm").fadeOut(1000);
+    $("#controls").fadeIn(1000);
+}
+
+function loadInfoScreen(){
+    var rand = insults[Math.floor(Math.random() * insults.length)];
+    $("#infoScreenText").html(rand);
+    $("#infoScreen").fadeIn(1000);
+}
+
+function changeColor(color){
+    $("#controlsPage").css("background-color", color);
+    $(".directionArrow").css("color", color);
+    $("#infoScreen").css("background-color", color);
+
+
+}
+
+
+let socket = new WebSocket("ws://132.181.13.96:2828", "checking Life Works");
 
 socket.onopen = function (event) {
   socket.sendBytes("hello");
