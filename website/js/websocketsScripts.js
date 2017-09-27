fb48ac20030b61b942a5cdbd82985f68e586fadd
Devin Davies <dda40@uclive.ac.nz>
2017-09-27 18:35:44 +1300

    changed ip to chris'

--- website/js/websocketsScripts.js
+++ website/js/websocketsScripts.js
@@ -62,7 +62,7 @@ createHeader = function (type, messageLength) {
  * Creates a WebSocket connection to the Game Recorder Server
  */
 function createGameRecorderSocket() {
-    gameRecorderSocket = new WebSocket("wss://132.181.16.17:2827"); // 2827 is the port game recorder runs on
+    gameRecorderSocket = new WebSocket("wss://techsols.homeip.net:2827"); // 2827 is the port game recorder runs on
     gameRecorderSocket.binaryType = 'arraybuffer';
 
     gameRecorderSocket.onerror = function (event) {
